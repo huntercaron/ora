0090caf21a18046e1b931c8f138d9c9c668a5107
Hunter Caron <huntercaron@me.com>
2020-07-06 23:08:28 +0200

    adding examples for my boy

---
+++ components/toggleExample.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, useRef } from "react"
+import * as Tone from "tone"
+
+export function ToggleExample() {
+    // useState is where you store states of the component
+    const [active, setActive] = useState()
+    // useRef is where you store references to things, variables that aren't state related
+    // in this case, we'll store our Oscillator on here
+    const osc = useRef()
+
+    // useEffect runs code when a component loads or each time it renders
+    useEffect(() => {
+        osc.current = new Tone.Oscillator(440, "sine").toMaster()
+    }, [])
+    // ^ it runs the code based on what is inside this array
+
+    useEffect(() => {
+        Tone.start()
+        if (active) osc.current.start()
+        else osc.current.stop()
+    }, [active])
+    // this ^ code runs whenever 'active' changes
+
+    const handleToggle = () => {
+        setActive(!active)
+    }
+
+    return (
+        <div style={{ border: "1px solid #eee" }}>
+            This is a component with a button
+            <button onClick={handleToggle}>Turn {active ? "Off" : "On"}</button>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,28 @@
 import Head from "next/head"
+import * as Tone from "tone"
+import { ToggleExample } from "../components/toggleExample"
+
+export function ButtonExample(props) {
+    // this takes the values out of the props for use
+    const { startNote } = props
+    // this is a component, its scoped to itself and can be re-used infinite times
+    // this function triggers when 'onClick' on the button is triggered
+    const playSound = () => {
+        Tone.start()
+        var synth = new Tone.Synth().toMaster()
+
+        //play a middle 'C' for the duration of an 8th note
+        synth.triggerAttackRelease(startNote, "8n")
+    }
+
+    return (
+        <div style={{ border: "1px solid #eee" }}>
+            This is a component with a button that plays a note with the start
+            of {startNote}
+            <button onClick={playSound}>Play Sound</button>
+        </div>
+    )
+}
 
 function Home() {
     return (
@@ -7,7 +31,13 @@ function Home() {
                 <title>Ora</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>this is a div</h1>
+            <h1>this is a Heading on the index page</h1>
+
+            <div>
+                <ToggleExample />
+                <ButtonExample startNote={"B2"} />
+                <ButtonExample startNote={"C4"} />
+            </div>
         </div>
     )
 }
