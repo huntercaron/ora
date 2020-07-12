d951fbed898792688e0fec5c432eec65ca1086be
Hunter Caron <huntercaron@me.com>
2020-07-12 23:28:35 +0200

    i swear to god

--- next.config.js
+++
@@ -1,4 +0,0 @@
-const nextBuildId = require("next-build-id")
-module.exports = {
-    generateBuildId: async () => nextBuildId({ dir: __dirname }),
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import { DrumMachine } from "../components/DrumMachine"
+import { DrumMachine } from "../components/drumMachine"
 
 function Home() {
     return (
