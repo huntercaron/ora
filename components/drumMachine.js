a4445c93c7cb1ed327764813f2d1c5cae1c1a7dd
Hunter Caron <huntercaron@me.com>
2020-07-12 23:04:29 +0200

    drum machine attempt #1

---
+++ components/drumMachine.js
@@ -0,0 +1,96 @@
+import { useState, useEffect, useRef } from "react"
+import Tone from "tone"
+import { motion } from "framer-motion"
+import styles from "./drumMachine.module.css"
+
+const notes = ["hihat", "kick", "perc", "snare"]
+const columns = 12
+const columnArray = [...new Array(columns)].map((_, i) => i)
+const activeNotesDefault = new Array(columns).fill(
+    new Array(notes.length).fill(false)
+)
+
+const samples = notes.reduce((res, sample) => {
+    res[sample] = `assets/samples/${sample}.wav`
+    return res
+}, {})
+
+function Note(props) {
+    const { active, isColumnActive, onTap } = props
+
+    return (
+        <motion.div
+            onTap={onTap}
+            className={styles.note}
+            initial={{ scale: 1 }}
+            animate={{
+                scale: isColumnActive ? 1.15 : 1,
+                background: active ? "rgba(0,0,0,255)" : "rgba(0,0,0,0)",
+            }}
+            whileTap={{ scale: 0.9 }}
+        />
+    )
+}
+
+export function DrumMachine() {
+    const drum = useRef()
+    const [activeNotes, setActiveNotes] = useState(activeNotesDefault)
+    const [activeColumn, setActiveColumn] = useState(-1)
+
+    useEffect(() => {
+        drum.current = new Tone.Players(samples, {
+            release: 1,
+            volume: -10,
+        }).toMaster()
+    }, [])
+
+    useEffect(() => {
+        const loop = new Tone.Sequence(
+            (time, col) => {
+                setActiveColumn(col)
+                notes.forEach((_, i) => {
+                    if (activeNotes[col][i]) {
+                        var vel = Math.random() * 0.5 + 0.5
+                        drum.current.get(notes[i]).start(time, 0, "8n", 0, vel)
+                    }
+                })
+            },
+            columnArray,
+            "8n"
+        ).start(0)
+        return () => loop.dispose()
+    }, [activeNotes])
+
+    const toggleNote = (col, row) => {
+        setActiveNotes((prevState) => {
+            const newState = prevState.map((arr) => arr.slice(0))
+            newState[col][row] = !prevState[col][row]
+            return newState
+        })
+    }
+
+    return (
+        <motion.div
+            onClick={() => {
+                Tone.start()
+                Tone.Transport.start()
+            }}
+            className={styles.machine}
+            style={{ grid: `repeat(${notes.length}, 50px) / auto-flow` }}
+        >
+            {activeNotes.map((column, col) => {
+                return column.map((active, row) => {
+                    return (
+                        <Note
+                            key={`${col}+${row}`}
+                            active={active}
+                            isColumnActive={activeColumn === col}
+                            onTap={() => toggleNote(col, row)}
+                            column={col}
+                        />
+                    )
+                })
+            })}
+        </motion.div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,5 @@
 import Head from "next/head"
-import * as Tone from "tone"
-import { ToggleExample } from "../components/toggleExample"
-
-export function ButtonExample(props) {
-    // this is a component, its scoped to itself and can be re-used infinite times
-
-    // this takes the values out of the props for use
-    const { startNote } = props
-
-    // this function triggers when 'onClick' on the button is triggered
-    const playSound = () => {
-        Tone.start()
-        var synth = new Tone.Synth().toMaster()
-
-        //play a middle 'C' for the duration of an 8th note
-        synth.triggerAttackRelease(startNote, "8n", Tone.context.currentTime)
-    }
-
-    return (
-        <div style={{ border: "1px solid #eee" }}>
-            This is a component with a button that plays a note with the start
-            of {startNote}
-            <button onClick={playSound}>Play Sound</button>
-        </div>
-    )
-}
+import { DrumMachine } from "../components/DrumMachine"
 
 function Home() {
     return (
@@ -38,14 +13,8 @@ function Home() {
                 />
             </Head>
 
-            <h1>PeePee PooPoo</h1>
-
             <div>
-                <ToggleExample />
-
-                {/* With component props, we can easily made each control do something slightly different */}
-                <ButtonExample startNote={"B2"} />
-                <ButtonExample startNote={"C4"} />
+                <DrumMachine />
             </div>
         </div>
     )
